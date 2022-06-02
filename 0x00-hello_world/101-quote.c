bf92d689d7a762a92f201a129f30756c4842c151
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-02 10:22:53 -0700

    print function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  *
  * Function: print a statement without printf and puts
  *
- * Return 0 (Sucess)
+ * Return 1 (Sucess)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
