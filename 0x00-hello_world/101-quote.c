95c0c285af5c1333182f5f4e433233f008b41234
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-02 10:32:37 -0700

    print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
  */
 int main(void)
 {
-	if(printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")){
+	if (printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"))
+	{
 	}
 	return (1);
 }
