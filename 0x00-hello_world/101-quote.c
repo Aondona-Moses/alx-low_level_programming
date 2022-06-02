0d817a5c12359d73790859c8091bcce0f18a6aab
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-02 10:19:33 -0700

    print wothout put

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Function: print a statement without printf and puts
+ *
+ * Return 0 (Sucess)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
