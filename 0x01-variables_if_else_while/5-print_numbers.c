44ac40ca13248fe308dc143cfc51fa5a7d900e8e
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-03 14:19:19 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Function: print single digit numbers
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar(10);
+	return (0);
+}
