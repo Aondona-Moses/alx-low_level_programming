2a068b07fb077c414b9564250814e084e554de6e
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-03 14:54:58 +0100

    invert

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Function: print single digit numbers
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	int num1, num2, num3;
+
+	num1 = 48;
+	num2 = 48;
+	num3 = 48;
+
+	while (num < 58)
+	{
+
