33ea633b5082ad5b64feb6756f8cff020dae75a3
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-03 06:00:48 -0700

    all

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,14 +12,8 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch == 'q' || ch == 'e')
-		{
-			continue;
-		}
-		else
-		{
+		if (ch != 'q' || ch != 'e')
 			putchar(ch);
-		}
 	putchar('\n');
 	return (0);
 }
