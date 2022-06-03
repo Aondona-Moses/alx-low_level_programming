e02678ba64d71609970c836c9ab51616e01dc420
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-03 09:24:39 -0700

    numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,6 +15,28 @@ int main(void)
 	num2 = 48;
 	num3 = 48;
 
-	while (num < 58)
+	while (num1 < 58)
 	{
-
+		num2 = num1 + 1;
+		while (num2 < 58)
+		{
+			num3 = num2 +1;
+			while (num3 < 58)
+			{
+				putchar(num1);
+				putchar(num2);
+				putchar(num3);
+				if (num1 < 55 || num2 < 56 || num3 < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				num3++;
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar(10);
+	return (0);
+}
