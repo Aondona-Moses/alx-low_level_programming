0aad8c4a427d1b5f10cada4c92a482cd51962cc2
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-03 05:17:14 -0700

    letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,12 @@
  *
  * Function: Print alphabet in lowercase
  *
- * Return (0) (Success)
+ * Return: Always (0) (Success)
  */
 int main(void)
 {
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
 	return (0);
