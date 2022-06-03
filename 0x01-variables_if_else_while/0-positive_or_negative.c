dec2ec5065025a868e69ee9154cb0a43f60ae444
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-03 04:06:30 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%u is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%u is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%u is negative\n", n);
 	}
 	return (0);
 }
