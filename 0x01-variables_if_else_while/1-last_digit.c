61b9515d131b620cf8d640803f755ac649fcb5f8
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-03 04:25:09 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
-#include ,time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Function: print last digit of any number
+ *
+ * Return: Always (0) (Success)
+ */
 int main(void)
 {
 	int n;
