4587c92759cab777301aa3457179cf4d8b9fb6ad
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-09 01:14:44 +0100

    task 1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,5 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
-void print_alphabet(int num);
+
+void positive_or_negative(int num);
+int larget_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
+
 #endif
