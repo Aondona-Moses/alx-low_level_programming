d181d76f3581d613ac9ed0064e8c5a0c7f50e32a
aondona-moses <mr.aondonamoses@gmail.com>
2022-06-09 01:18:56 +0100

    task 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - test function that prints if integer is positive or negative
- *
- * Return: Always (0) (Success)
- */
+  * main - Test function for positive or negative
+  * Return: 0
+  */
 
 int main(void)
 {
-int num;
-	
-num = 0;
-positive_or_negative(num);
+int i;
+
+i = 0;
+positive_or_negative(i);
 
 return (0);
 }
